662ec7f568bf268ab9b616e5aaa65b8b0fe472b2
shahbuland <smatiana1@gmail.com>
2025-05-04 14:22:23 -0400

    Positional encoding

---
+++ diffusable_autoencoders/nn/embeddings.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+import einops as eo
+
+class ImageRoPE(nn.Module):
+    def __init__(self, config : 'TransformerConfig'):
+        super().__init__()
+
+        pass
+
+    def forward(self, q, k):
+        # q k both [b,h,n,d]
+        pass
+
+class LearnedPosEnc(nn.Module):
+    def __init__(self, n_seq, dim):
+        super().__init__()
+
+        self.p = nn.Parameter(torch.randn(n_seq,dim)*0.02)
+
+    def forward(self, x):
+        b,n,d = x.shape
+        p = eo.repeat(self.p, 'n d -> b n d', b = b)
+        return x + p
