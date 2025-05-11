73928ed600643430696f6a4a58c6c7cbc28fc013
shahbuland <smatiana1@gmail.com>
2025-05-11 18:00:25 -0400

    bla

--- diffusable_autoencoders/data/__init__.py
+++ diffusable_autoencoders/data/__init__.py
@@ -1,6 +1,7 @@
 from . import mnist
-
+from . import local_imagenet_256
 def get_loader(data_id, batch_size):
     if data_id == "mnist":
         return mnist.get_loader(batch_size)
-    
+    if data_id == "local_imagenet_256":
+        return local_imagenet_256.get_loader(batch_size)
