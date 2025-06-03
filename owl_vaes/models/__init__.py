1009b8f027ea97650cd073b275213049cf34cba2
shahbuland <smatiana1@gmail.com>
2025-06-03 23:57:08 +0000

    fix some stuff

--- owl_vaes/models/__init__.py
+++ owl_vaes/models/__init__.py
@@ -1,11 +1,12 @@
 from typing import Any
 
 from .dcae import DCAE
+
+from .dcae import DCAE, Decoder
 from .dcae_vq import DCVQVAE
 from .proxy_titok import ProxyTiToKVAE
 from .titok import TiToKVAE
 from .titok_vq import TiToKVQVAE
-
 
 def get_model_cls(model_id: str) -> Any:
     return {
@@ -14,4 +15,5 @@ def get_model_cls(model_id: str) -> Any:
         "titok_vq": TiToKVQVAE,
         "dcae_vq": DCVQVAE,
         "proxy_titok": ProxyTiToKVAE,
+        "dcae_decoder": Decoder
     }.get(model_id)
