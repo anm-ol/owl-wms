d512b71e6b7bce1b9f00a55a6f99893e657565cb
shahbuland <smatiana1@gmail.com>
2025-06-06 04:03:30 +0000

    move imports so dependencies are safer

--- owl_vaes/models/__init__.py
+++ owl_vaes/models/__init__.py
@@ -1,22 +1,25 @@
 from typing import Any
 
-from owl_vaes.nn.audio import AudioAutoEncoder
-
-from .dcae import DCAE
-
-from .dcae import DCAE, Decoder
-from .dcae_vq import DCVQVAE
-from .proxy_titok import ProxyTiToKVAE
-from .titok import TiToKVAE
-from .titok_vq import TiToKVQVAE
-
 def get_model_cls(model_id: str) -> Any:
-    return {
-        "dcae": DCAE,
-        "titok": TiToKVAE,
-        "titok_vq": TiToKVQVAE,
-        "dcae_vq": DCVQVAE,
-        "proxy_titok": ProxyTiToKVAE,
-        "dcae_decoder": Decoder
-        "audio_ae": AudioAutoEncoder,
-    }.get(model_id)
+    if model_id == "dcae":
+        from .dcae import DCAE
+        return DCAE
+    if model_id == "titok":
+        from .titok import TiToKVAE
+        return TiToKVAE
+    if model_id == "titok_vq":
+        from .titok_vq import TiToKVQVAE
+        return TiToKVQVAE
+    if model_id == "dcae_vq":
+        from .dcae_vq import DCVQVAE
+        return DCVQVAE
+    if model_id == "proxy_titok":
+        from .proxy_titok import ProxyTiToKVAE
+        return ProxyTiToKVAE
+    if model_id == "dcae_decoder":
+        from .dcae import Decoder
+        return Decoder
+    if model_id == "audio_ae":
+        from owl_vaes.nn.audio import AudioAutoEncoder
+        return AudioAutoEncoder
+    return None
