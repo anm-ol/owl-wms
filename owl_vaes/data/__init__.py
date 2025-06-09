4ab27664afe9184dd066d8cdb60d86152b4036dd
shahbuland <smatiana1@gmail.com>
2025-06-09 13:30:00 -0400

    add dataloader to init

--- owl_vaes/data/__init__.py
+++ owl_vaes/data/__init__.py
@@ -1,4 +1,4 @@
-def get_loader(data_id: str, batch_size: int, filepath: str | list[str] | None = None):
+def get_loader(data_id: str, batch_size: int, **data_kwargs):
     if data_id == "mnist":
         from . import mnist
         return mnist.get_loader(batch_size)
@@ -14,4 +14,7 @@ def get_loader(data_id: str, batch_size: int, filepath: str | list[str] | None =
     if data_id == "audio_loader":
         from .audio_loader import get_audio_loader
         assert filepath is not None, "No filepath provided for the dataset."
-        return get_audio_loader(batch_size, filepath)
+        return get_audio_loader(batch_size, data_kwargs['filepath'])
+    if data_id == "s3_cod":
+        from . import s3_cod
+        return s3_cod.get_loader(batch_size, **data_kwargs)
