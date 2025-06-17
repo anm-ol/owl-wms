88d61e900b231b28a0e654dc480c8624acee226c
shahbuland <smatiana1@gmail.com>
2025-06-17 13:22:42 -0400

    add transformer block causal decoder with kv cache

--- owl_vaes/nn/embeddings.py
+++ owl_vaes/nn/embeddings.py
@@ -2,6 +2,10 @@ import torch
 from torch import nn
 
 import einops as eo
+from rotary_embedding_torch import (
+    RotaryEmbedding,
+    apply_rotary_emb
+)
 
 class ImageRoPE(nn.Module):
     def __init__(self, config : 'TransformerConfig'):
@@ -23,3 +27,23 @@ class LearnedPosEnc(nn.Module):
         b,n,d = x.shape
         p = eo.repeat(self.p, 'n d -> b n d', b = b)
         return x + p
+
+class RoPEEmbedding(nn.Module):
+    """
+    Video RoPE embedding for when latents are 3D [n,h,w]
+    """
+    def __init__(self, config : 'TransformerConfig'):
+        super().__init__()
+
+        dim_head = config.d_model // config.n_heads
+        self.pos_emb = RotaryEmbedding(
+            dim = dim_head//2
+        )
+
+    def forward(self, q, k):
+        # q k both [b,h,n,d]
+        # k can be longer for caching
+        q,k = self.rotate_queries_with_cached_keys(q,k)
+        return q,k
+        
+        return q, k
