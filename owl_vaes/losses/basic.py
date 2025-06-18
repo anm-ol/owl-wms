0bd47f5614cfad524d3cd9d5bf75f7ce05caedb9
shahbuland <smatiana1@gmail.com>
2025-06-18 21:36:48 +0000

    add real kl and new model

--- owl_vaes/losses/basic.py
+++ owl_vaes/losses/basic.py
@@ -11,9 +11,10 @@ def latent_reg_loss(z: Tensor, target_var: float = 0.1) -> Tensor:
     # z is [b,c,h,w]
     # KL divergence between N(z, 0.1) and N(0,1)
     mu = z
-    log_target_var = 2 * torch.log(
-        torch.tensor(target_var, device=z.device, dtype=z.dtype)
-    )  # log(0.1^2)
+    log_target_var = 2*torch.log(z.float().std())
+    #log_target_var = 2 * torch.log(
+    #    torch.tensor(target_var, device=z.device, dtype=z.dtype)
+    #)  # log(0.1^2)
 
     kl = -0.5 * (1 + log_target_var - mu.pow(2) - log_target_var.exp())
     kl = einops.reduce(kl, "b ... -> b", reduction="sum").mean()
