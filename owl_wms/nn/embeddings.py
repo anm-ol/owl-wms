fd1a20cf5af2a2323832484bcbabb5a27646f2e6
Andrew Lapp <andrew@github.rew.la>
2025-08-26 12:18:11 -0400

    fix warnings

--- owl_wms/nn/embeddings.py
+++ owl_wms/nn/embeddings.py
@@ -35,7 +35,7 @@ class SinCosEmbed(nn.Module):
         self.theta = theta
         self.mult = mult
 
-    @autocast(enabled=False)
+    @autocast("cuda", enabled=False)
     def forward(self, x):
         x = x.float()
         # Handle different input types
